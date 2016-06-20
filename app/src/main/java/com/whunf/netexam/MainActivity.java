b8cda8301f3e28f6462c1f2e5a72afb58f430fae
abc2016qwe <abc2016qwe@sohu.com>
2016-06-20 17:38:24 +0800

    add an log

--- app/src/main/java/com/whunf/netexam/MainActivity.java
+++ app/src/main/java/com/whunf/netexam/MainActivity.java
@@ -3,6 +3,7 @@ package com.whunf.netexam;
 import android.content.Context;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
@@ -37,7 +38,7 @@ public class MainActivity extends AppCompatActivity {
         mJokeAdapter = new JokeAdapter(this);
         listview.setAdapter(mJokeAdapter);
         initData();
-
+        Log.d(TAG, "onCreate() called with: " + "savedInstanceState = [" + savedInstanceState + "]");
     }
 
     StringRequest sr;
